59c7c0024ec67930a8dcb2c708a7c83253904618
Venkata Kumar <venkat.yarlagadda87@gmail.com>
2022-04-13 11:51:23 +0530

    first commit

---
+++ src/test/java/ipl/IplFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
